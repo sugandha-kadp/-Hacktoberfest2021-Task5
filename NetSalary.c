8723e585cf967bc4367875ae67410685d4d696b6
Sugandha <72730060+Sugandha-kadp@users.noreply.github.com>
2021-10-03 20:54:02 +0530

    Create NetSalary.c
    This Repo has some issues, fix that issues

---
+++ NetSalary.c
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main(void)
+{
+ double salary, netSalary;
+ int etype, otHrs, otRate;
+
+  // Emp types are 1,2,3
+ cout << "Enter Employee Type : "
+ cout >> salary;
+
+ cout << "Enter Salary : ";
+ cin >> salary;
+
+ cout << "Enter OtHrs : ";
+ cin >> otHrs;
+
+  switch (salary)
+  {
+     case 1 :
+       otRate = 1000;
+      break;
+     case 8 :
+        otRate = 1500;
+     default :
+        otRate = 1700;
+      break;
+  }
+ netSalary = salary + otHrs* otRate;
+ cout << "Net Salary is "<<  
+
+ return 0;
+}
