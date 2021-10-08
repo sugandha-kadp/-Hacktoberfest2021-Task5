140a8960240d07d8baf0157883f0cac7c3ce49da
Kumaran <sathiyavarathankumaran@gmail.com>
2021-10-08 20:06:27 +0530

    Update NetSalary

--- NetSalary.c
+++ NetSalary.c
@@ -4,30 +4,44 @@ int main(void)
 {
  double salary, netSalary;
  int etype, otHrs, otRate;
+ bool i = true;
 
   // Emp types are 1,2,3
- cout << "Enter Employee Type : "
- cout >> salary;
+ cout << "Enter Employee Type : ";
+ cin >> etype;
 
  cout << "Enter Salary : ";
  cin >> salary;
 
  cout << "Enter OtHrs : ";
  cin >> otHrs;
-
-  switch (salary)
+while(i)
+{
+  i = true;
+  switch (etype)
   {
      case 1 :
        otRate = 1000;
+       i = false;
       break;
-     case 8 :
+     case 2 :
         otRate = 1500;
-     default :
+        i = false;
+        break;
+     case 3 :
         otRate = 1700;
+        i = false;
+        break;
+     default :
+        cout << "Enter valid employee type" << endl;
+        cout << "Enter Employee Type : ";
+        cin >> etype;
+        i = true;
       break;
   }
+}
  netSalary = salary + otHrs* otRate;
- cout << "Net Salary is "<<  
+ cout << "Net Salary is "<<  netSalary;
 
  return 0;
 }
