1c8d1bc82414a6fc7189fd7770498c127257c045
Maleesha Dinoli Silva <56461176+maleeshasilva@users.noreply.github.com>
2021-10-08 17:24:06 +0530

    added the solution code

---
+++ Updated_NetSalary.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+ double salary, netSalary;
+ int etype, otHrs, otRate;
+
+  // Emp types are 1,2,3
+ printf("Enter Employee Type : ");
+ scanf("%d", &etype);
+
+ printf("Enter Salary : ");
+ scanf("%lf", &salary);
+
+ printf("Enter OtHrs : ");
+ scanf("%d", &otHrs); 
+
+  switch (etype)
+  {
+     case 1 :
+       otRate = 1000;
+      break;
+     case 8 :
+        otRate = 1500;
+     default :
+        otRate = 1700;
+      break;
+  }
+ netSalary = salary + otHrs* otRate;
+ printf("Net Salary is :%lf ", netSalary);
+
+ return 0;
+}
