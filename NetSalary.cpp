8eb15224c5becf222f111b52969613f182e7d554
Bishal <bishallaha12345@gmail.com>
2021-10-09 00:10:14 +0530

    resolved all errors

---
+++ NetSalary.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+	
+	double salary, netsalary;
+	int emptype,hour,rate;
+	
+	printf("Enter Employee Type --- ");
+	scanf("%d",&emptype);
+	printf("Enter Employee Salary --- ");
+	scanf("%lf",&salary);
+	printf("Enter Hours --- ");
+	scanf("%d",&hour);
+	
+	switch(emptype){
+		
+		case 1 :
+       			rate = 1000;
+      			break;
+     	case 8 :
+        		rate = 1500;
+     			break;
+		default :
+        		rate = 1700;
+      			break;
+	}
+	
+	netsalary = salary + hour*rate;
+ 	printf("Netsalary is --- %lf",netsalary);	
+ 	
+ 	return 0;
+}
